91559277bb5dbf413baf90fdce8ae0e08b55ce2e
Reynaldo Putra <reynaldo.informatic@engineer.com>
2023-10-25 15:04:38 +0700

    update composer.json, update package.json, update tailwind.config.js, update app.js, update app.css

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,20 +1,32 @@
-import defaultTheme from 'tailwindcss/defaultTheme';
-import forms from '@tailwindcss/forms';
-import typography from '@tailwindcss/typography';
+import defaultTheme from "tailwindcss/defaultTheme";
+import forms from "@tailwindcss/forms";
+import typography from "@tailwindcss/typography";
+
+const colors = require("tailwindcss/colors");
 
 /** @type {import('tailwindcss').Config} */
 export default {
     content: [
-        './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
-        './vendor/laravel/jetstream/**/*.blade.php',
-        './storage/framework/views/*.php',
-        './resources/views/**/*.blade.php',
+        "./vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php",
+        "./vendor/laravel/jetstream/**/*.blade.php",
+        "./storage/framework/views/*.php",
+        "./resources/views/**/*.blade.php",
     ],
 
     theme: {
         extend: {
             fontFamily: {
-                sans: ['Figtree', ...defaultTheme.fontFamily.sans],
+                sans: ["Inter", ...defaultTheme.fontFamily.sans],
+            },
+
+            colors: {
+                dark: {
+                    "eval-0": "#151823",
+                    "eval-1": "#222738",
+                    "eval-2": "#2A2F42",
+                    "eval-3": "#2C3142",
+                },
+                cyan: colors.cyan,
             },
         },
     },
